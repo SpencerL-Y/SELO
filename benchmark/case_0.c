9424186a9a1321fab0ca1357c84b40689d0d2909
clexmaAtFlankerB <lixie19@ios.ac.cn>
2024-10-11 23:16:26 +0800

    stage for merge

--- benchmark/case_0.c
+++ benchmark/case_0.c
@@ -1,8 +1,37 @@
 #include <stdlib.h>
-
-int main(){
-    int * data = malloc(sizeof(int));
-    int * data2 = malloc(2*sizeof(int));  
-    free(data);
-    int i = *(data + 1);
+extern int __VERIFIER_nondet_int(void);
+struct item {
+    struct item *next;
+    struct item *data;
+};
+static void append(struct item **plist)
+{
+    struct item *item = malloc(sizeof *item);
+    item->next = *plist;
+    item->data = (item->next)
+        ? item->next->data
+        : malloc(sizeof *item);
+    *plist = item;
 }
+int main()
+{
+    struct item *list = ((void *)0);
+    append(&list);
+    append(&list);
+    if (list) {
+        struct item *next = list->next;
+        free(list->data);
+        free(list);
+        list = next;
+    }
+    if (list) {
+        struct item *next = list->next;
+        if (!next)
+            free(list->data);
+        free(list);
+        list = next;
+    }
+    return 0;
+}
+
+// invalid free  17
