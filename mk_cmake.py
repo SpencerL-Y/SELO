430cad79d94a67ebcb16d3f9d4be95c5f2f9e3b4
clexmaAtFlankerB <lixie19@ios.ac.cn>
2024-10-12 18:10:02 +0800

    update script

--- mk_cmake.py
+++ mk_cmake.py
@@ -1,4 +1,17 @@
 import os
 import sys
 
-os.system("cd build && rm -rf * && cmake .. -GNinja -DBUILD_TESTING=On -DENABLE_REGRESSION=On $ESBMC_CLANG  -DENABLE_Z3_SLHV=On -DZ3_DIR=/home/clexma/Desktop/fox3/slhv/Z3-slhv/z3_slhv_lib  -DCMAKE_INSTALL_PREFIX:PATH=$PWD/../../release ")
+your_z3_lib_path = "/home/clexma/Desktop/fox3/slhv/Z3-slhv/z3_slhv_lib"
+your_release_path = "$PWD/../../release" 
+is_static = sys.argv[1]
+if "--static" in  is_static:
+
+    os.system("cd build && rm -rf * && cmake .. -GNinja -DBUILD_TESTING=On -DENABLE_REGRESSION=On $ESBMC_CLANG  -DBUILD_STATIC=${ESBMC_STATIC:-ON} -DENABLE_Z3_SLHV=On -DZ3_DIR=" + your_z3_lib_path + " -DCMAKE_INSTALL_PREFIX:PATH=" + your_release_path)
+elif "--shared" in is_static:
+    os.system("cd build && rm -rf * && cmake .. -GNinja -DBUILD_TESTING=On -DENABLE_REGRESSION=On $ESBMC_CLANG  -DENABLE_Z3_SLHV=On -DZ3_DIR=" + your_z3_lib_path + "   -DCMAKE_INSTALL_PREFIX:PATH=" + your_release_path)
+else:
+    print("need argument: --static or --shared")
+
+
+
+
