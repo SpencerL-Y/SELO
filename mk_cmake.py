63b09760f383d29d33cf767b391dab1a6e2475ce
zhuyt <zhuyutian57@gmail.com>
2024-10-14 11:07:27 +0800

    add build.sh

--- mk_cmake.py
+++
@@ -1,4 +0,0 @@
-import os
-import sys
-
-os.system("cd build && rm -rf * && cmake .. -GNinja -DBUILD_TESTING=On -DENABLE_REGRESSION=On $ESBMC_CLANG  -DENABLE_Z3_SLHV=On -DZ3_DIR=/home/clexma/Desktop/fox3/slhv/Z3-slhv/z3_slhv_lib  -DCMAKE_INSTALL_PREFIX:PATH=$PWD/../../release ")
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,33 +1,14 @@
 #!/bin/sh
 
-# Set arguments that should be available for every OS
-BASE_ARGS="\
-    -DDOWNLOAD_DEPENDENCIES=On \
-    -GNinja \
-    -DENABLE_CSMITH=On \
-    -DBUILD_TESTING=On \
-    -DENABLE_REGRESSION=On \
-    -DENABLE_SOLIDITY_FRONTEND=On \
-    -DENABLE_JIMPLE_FRONTEND=On \
-    -DCMAKE_INSTALL_PREFIX:PATH=$PWD/release \
-"
-# Must disable old frontend to enable goto contractor. github issue #1110
-# https://github.com/esbmc/esbmc/issues/1110.
-SOLVER_FLAGS="\
-    -DENABLE_BOOLECTOR=On \
-    -DENABLE_YICES=Off \
-    -DENABLE_CVC4=OFF \
-    -DENABLE_BITWUZLA=On \
-    -DENABLE_GOTO_CONTRACTOR=On \
-    -DACADEMIC_BUILD=Off \
-"
+# Set arguments
+BASE_ARGS="-GNinja"
+
+SOLVER_FLAGS="-DENABLE_Z3_SLHV=On"
 
 COMPILER_ARGS=''
 
 STATIC=
 CLANG_VERSION=11
-
-ARCH=`uname -m`
 
 error() {
     echo "error: $*" >&2
@@ -37,10 +18,8 @@ error() {
 # Ubuntu setup (pre-config)
 ubuntu_setup () {
     
-    
     # Tested on ubuntu 22.04
     PKGS="\
-        clang-$CLANG_VERSION clang-tidy-$CLANG_VERSION \
         python-is-python3 csmith python3 \
         git ccache unzip wget curl libcsmith-dev gperf \
         libgmp-dev cmake bison flex g++-multilib linux-libc-dev \
@@ -48,100 +27,78 @@ ubuntu_setup () {
         libtinfo-dev pkg-config python3-pip python3-toml \
         openjdk-11-jdk \
     "
-    if [ -z "$STATIC" ]; then STATIC=ON; fi
-    if [ $STATIC = OFF ]; then
+
+    # Create deps for Clang and Z3_SLHV
+    if [ ! -d "deps" ]; then
+        echo "Create deps directory"
+        mkdir deps
+    fi
+    cd deps
+
+    if [ -z "$STATIC" ]; then
+        STATIC=static
+    else
+        STATIC=shared
+    fi
+
+    echo "Configuring Clang-$CLANG_VERSION: $STATIC"
+    if [ $STATIC = shared ]; then
         PKGS="$PKGS \
             llvm-$CLANG_VERSION-dev \
             libclang-$CLANG_VERSION-dev \
             libclang-cpp$CLANG_VERSION-dev \
-            libz3-dev \
         "
         BASE_ARGS="$BASE_ARGS \
             -DClang_DIR=/usr/lib/cmake/clang-$CLANG_VERSION \
             -DLLVM_DIR=/usr/lib/llvm-$CLANG_VERSION/lib/cmake/llvm \
-            -DZ3_DIR=/usr \
         "
-        echo "Configuring shared Ubuntu build with Clang-$CLANG_VERSION frontend"
     else
-        echo "Configuring static Ubuntu build"
+        if [ ! -d "clang+llvm-11" ]; then
+            wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/clang+llvm-11.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
+            tar xfJ clang+llvm-11.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
+            mv clang+llvm-11.0.0-x86_64-linux-gnu-ubuntu-20.04 clang+llvm-11
+        fi
+        BASE_ARGS="$BASE_ARGS -DClang_DIR=$PWD/clang+llvm-11 -DLLVM_DIR=$PWD/clang+llvm-11"
     fi
 
-    if [ $ARCH = "aarch64" ]
-    then
-	echo "Detected ARM64 Linux!"
-	# TODO: We should start using container builds in actions!
-	SOLVER_FLAGS="$SOLVER_FLAGS -DENABLE_Z3=On -DZ3_DIR=/usr -DENABLE_GOTO_CONTRACTOR=OFF"
-	return
+
+    echo "Configuring Z3_SLHV: $STATIC"
+    if [ ! -d "Z3-SLHV" ]; then
+        git clone https://github.com/SpencerL-Y/Z3-SLHV.git
     fi
+    cd Z3-SLHV
+    if [ ! -d "z3_slhv_lib" ]; then
+        rm -r build && python3 mk_${STATIC}_cmake.py
+        cd build && ninja install
+    fi
+    BASE_ARGS="$BASE_ARGS -DBUILD_STATIC=$STATIC"
+    SOLVER_FLAGS="$SOLVER_FLAGS \
+        -DZ3_DIR=$PWD/z3_slhv_lib \
+        -DCMAKE_CXX_FLAGS=\"-I$PWD/z3_slhv_lib/include\" \
+    "
     
     sudo apt-get update &&
     sudo apt-get install -y $PKGS &&
     echo "Installing Python dependencies" &&
     pip3 install --user meson ast2json &&
-    meson --version &&
-
-    BASE_ARGS="$BASE_ARGS \
-        -DENABLE_OLD_FRONTEND=Off \
-        -DENABLE_PYTHON_FRONTEND=On \
-        -DBUILD_STATIC=$STATIC \
-    " &&
-    SOLVER_FLAGS="$SOLVER_FLAGS \
-        -DENABLE_Z3=ON \
-    " 
-}
-
-ubuntu_post_setup () {
-  echo "No further steps needed for ubuntu"
-}
-
-# macOS setup (pre-config)
-macos_setup () {
-    echo "Configuring macOS build"
-    if [ -z "$STATIC" ]; then STATIC=OFF; fi
-    if [ $STATIC = ON ]; then
-        error "static macOS build is currently not supported"
-    fi
-    brew install z3 gmp csmith cmake boost ninja python3 automake bison flex &&
-    pip3 install PySMT toml ast2json &&
-    wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/clang+llvm-11.0.0-x86_64-apple-darwin.tar.xz &&
-    tar xf clang+llvm-11.0.0-x86_64-apple-darwin.tar.xz &&
-    mv clang+llvm-11.0.0-x86_64-apple-darwin ../clang &&
-    BASE_ARGS="$BASE_ARGS \
-        -DENABLE_WERROR=Off \
-        -DBUILD_STATIC=$STATIC \
-        -DClang_DIR=$PWD/../clang \
-        -DLLVM_DIR=$PWD/../clang \
-        -DC2GOTO_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk \
-    "
-}
-
-# macOS needs an extra step before testing
-macos_post_setup () {
-  chmod +x build/macos-wrapper.sh
+    meson --version
 }
 
 usage() {
     echo "$0 [-OPTS]"
     echo
+    echo "THe script should be executed in ./script/"
     echo "Options [defaults]:"
     echo "  -h         display this help message"
     echo "  -b BTYPE   set cmake build type to BTYPE [RelWithDebInfo]"
-    echo "  -s STYPE   enable sanitizer STYPE and compile with clang [disabled]"
+    echo "  -c         enable clang [disabled]"
     echo "  -e ON|OFF  enable/disable -Werror [OFF]"
-    echo "  -r ON|OFF  enable/disable 'benchbringup' [OFF]"
-    echo "  -d         enable debug output for this script and c2goto"
-    echo "  -S ON|OFF  enable/disable static build [ON for Ubuntu, OFF for macOS]"
-    echo "  -c VERS    use packaged clang-VERS in a shared build on Ubuntu [11]"
-    echo "  -C         build an SV-COMP version [disabled]"
-    echo "  -B ON|OFF  enable/disable esbmc bundled libc [ON]"
+    echo "  -S ON|OFF  enable/disable static build [ON]"
     echo
     echo "This script prepares the environment, downloads dependencies, configures"
-    echo "the ESBMC build and runs the commands to compile and install ESBMC into"
-    echo "the directory: $PWD/release"
-    echo "Needs to be executed from the top-level directory of ESBMC's source tree."
-    echo "The build directory 'build' must not exist and will be created by this script."
+    echo "the SELO build and runs the commands to compile."
     echo
-    echo "Supported environments are: Ubuntu-22.04 and macOS."
+    echo "Supported environments: Ubuntu-22.04"
 }
 
 # Setup build flags (release, debug, sanitizer, ...)
@@ -153,21 +110,7 @@ do
     s) BASE_ARGS="$BASE_ARGS -DSANITIZER_TYPE=${OPTARG}"
        COMPILER_ARGS="$COMPILER_ARGS CC=clang CXX=clang++" ;;
     e) BASE_ARGS="$BASE_ARGS -DENABLE_WERROR=${OPTARG}" ;;
-    r) BASE_ARGS="$BASE_ARGS -DBENCHBRINGUP=${OPTARG}" ;;
-    d) set -x; export ESBMC_OPTS='--verbosity 9' ;;
     S) STATIC=$OPTARG ;; # should be capital ON or OFF
-    c) CLANG_VERSION=$OPTARG ;; # LLVM/Clang major version
-    C) BASE_ARGS="$BASE_ARGS -DESBMC_SVCOMP=ON"
-       SOLVER_FLAGS="\
-          -DENABLE_BOOLECTOR=On \
-    	  -DENABLE_YICES=ON \
-	  -DENABLE_CVC4=OFF \
-          -DENABLE_BITWUZLA=On \
-	  -DENABLE_Z3=On \
-          -DENABLE_Mathsat=ON \
-          -DENABLE_GOTO_CONTRACTOR=On \
-          -DACADEMIC_BUILD=ON"  ;;
-    B) BASE_ARGS="$BASE_ARGS -DESBMC_BUNDLE_LIBC=$OPTARG" ;;
     *) exit 1 ;;
     esac
 done
@@ -176,37 +119,27 @@ if [ $# -ge $OPTIND ]; then
     error "unknown trailing parameters: $*"
 fi
 
-# Detect the platform ($OSTYPE was not working on github actions for ubuntu)
-# Note: Linux here means Ubuntu, this will mostly not work anywhere else.
-OS=`uname`
+ROOT=$PWD
+if [ `basename $ROOT` != "SELO" ]; then
+    error "The script must be executed in root of SELO"
+fi
+
+# Setup Clang+LLVM and Z3_SLHV
+ubuntu_setup
+
+echo "Build SELO"
+cd $ROOT
 
 # Create build directory
-mkdir build && cd build || exit $?
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+cd build && rm -rf *
 
-case $OS in
-  'Linux')
-    ubuntu_setup
-    ;;
-  'Darwin')
-    macos_setup
-    ;;
-  *) echo "Unsupported OS $OSTYPE" ; exit 1; ;;
-esac || exit $?
-
-# Configure ESBMC
 printf "Running CMake:"
 printf " '%s'" $COMPILER_ARGS cmake .. $BASE_ARGS $SOLVER_FLAGS
 echo
-$COMPILER_ARGS cmake .. $BASE_ARGS $SOLVER_FLAGS &&
-# Compile ESBMC
-cmake --build . && ninja install || exit $?
+$COMPILER_ARGS cmake .. $BASE_ARGS $SOLVER_FLAGS
 
-case $OS in
-  'Linux')
-    ubuntu_post_setup
-    ;;
-  'Darwin')
-    macos_post_setup
-    ;;
-  *) echo "Unsupported OS $OSTYPE" ; exit 1; ;;
-esac || exit $?
+# Compile SELO
+cmake --build .
